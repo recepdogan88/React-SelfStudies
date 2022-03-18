d3a12b45739d007d6bae7fcb63a20cdbfb64af6a
unknown <rdogan8891gmail.com>
2022-03-18 21:23:35 +0100

    fake api has been created

--- Movies-App/movies/src/App.js
+++ Movies-App/movies/src/App.js
@@ -1,58 +1,26 @@
 import './App.css';
-import {useState} from "react"
+import {useState,useEffect} from "react"
 import MovieList from './components/MovieList';
 import SearchBar from './components/SearchBar';
  
 function App() {
   const [search,setSearch]=useState("")
-  const [movies,setmovies]=useState([
-    {
-        "id": 1,
-        "name": "The Flash",
-        "rating": 8.3,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-    },
+  const [movies,setmovies]=useState([])
 
-    {
-        "id": 2,
-        "name": "Interstellar",
-        "rating": 6.8,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-    },
+async function getData(){
+    const baseURL="http://localhost:3001/movies"
+    const response= await fetch(baseURL)
+    const data=await response.json()
+    setmovies(data)
+    console.log(movies)
+  }
+  
+useEffect(() => {
+    getData()
+},[]);
+ 
+console.log(movies)
 
-    {
-        "id": 3,
-        "name": "Arrow",
-        "rating": 7.9,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-    }, {
-        "id": 4,
-        "name": "Rogue",
-        "rating": 7.4,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg"
-    },
-
-    {
-        "id": 5,
-        "name": "Project Power",
-        "rating": 6.7,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg"
-    },
-
-    {
-        "id": 6,
-        "name": "Superman",
-        "rating": 7.6,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/6Bbq8qQWpoApLZYWFFAuZ1r2gFw.jpg"
-    }
-]
-)
 function searchMovie(e){
     setSearch(e.target.value)
 }
