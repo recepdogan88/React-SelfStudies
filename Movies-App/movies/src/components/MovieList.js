548dfed0835627e6a85fefd17a4439767a7a4f83
unknown <rdogan8891gmail.com>
2022-03-18 16:04:11 +0100

    cards have been added

---
+++ Movies-App/movies/src/components/MovieList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function MovieList({movies}) {
+  return (
+      <div className='row'>
+      {movies.map(movie =>(
+      <div className='col-lg-4'>
+        <div className="card mb-4 shadow-sm" >
+          <img src={movie.imageURL} className="card-img-top" alt="..."/>
+          <div className="card-body">
+              <h5 className="card-title">{movie.name}</h5>
+              <p className="card-text">{movie.overview}</p>
+              <div className='d-flex justify-content-between align-items-center'>
+                <button type='button' className='btn btn-md btn-outline-danger'>DELETE</button>
+                <h2><span className='badge bg-info'>{movie.rating}</span></h2>
+              </div>
+          </div>
+          </div>
+      </div>
+     ))}
+      </div>
+  )
+}
